213114a85b77dc307f2a115eed36824a64f6a745
temikartemoff <outlooking@icloud.com>
2023-02-25 12:21:24 +0400

    feat: add answer

--- src/index.js
+++ src/index.js
@@ -38,8 +38,29 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let resArray = [];
+    for (let i = 0; i < expr.length; i += 10) {
+      resArray.push(expr.slice(i, i + 10)); 
+    }    
+    let col = [];
+    for (elem of resArray) {
+      let sum = 0;
+      for (let i=0; i<elem.indexOf('1'); i++) {  
+        sum += 1
+      }  
+      col.push(elem.slice(sum));
+    }
+    let morseCode = (col.join().replaceAll('10', '.').replaceAll('11', '-').replaceAll('**********', ' ')).split(',');    
+    let decoded = [];
+      for(let x = 0; x < morseCode.length; x++){
+          if(MORSE_TABLE[morseCode[x]]){
+              decoded.push( MORSE_TABLE[morseCode[x]] );
+          }  else if (morseCode[x] === ' ') {
+              decoded.push(' ');
+          }
+      }
+    return decoded.join('').toLowerCase();
+};
 
 module.exports = {
     decode
